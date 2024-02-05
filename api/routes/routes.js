5adeb83453975bcf21c8a9ff85deea1b7d2ddc34
shuerry <105619406+shuerry@users.noreply.github.com>
2024-02-05 18:01:11 -0500

    Initial commit

---
+++ api/routes/routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import Controller from '../controllers/controllers.js';
+
+const router = express.Router;
+
+router.route("/").get((req, res) => {
+    //umm something
+});
+
+export default router;
