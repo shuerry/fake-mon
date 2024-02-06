ac7aab0d607e15cfc727286ca04cc641576b567a
shuerry <105619406+shuerry@users.noreply.github.com>
2024-02-06 17:43:39 -0500

    Additional Pages added

--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -4,6 +4,14 @@ class PageController {
     static getHomePage(req, res) {
         res.sendFile(path.resolve() + "/index.html");
     }
+
+    static getArticles(req, res) {
+        res.sendFile(path.resolve() + "/articles.html");
+    }
+
+    static getProfile(req, res) {
+        res.sendFile(path.resolve() + "/profile.html");
+    }
 }
 
 export default PageController;
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.route("/").get(PageController.getHomePage);
 
+router.route("/articles").get(PageController.getArticles);
+
+router.route("/profile").get(PageController.getProfile);
+
 export default router;
