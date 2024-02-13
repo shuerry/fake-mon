149281ceaceda9e419d3660e91006f3a44cdff0e
shuerry <105619406+shuerry@users.noreply.github.com>
2024-02-13 17:41:24 -0500

    Added ejs

--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -6,7 +6,25 @@ class PageController {
     }
 
     static getArticles(req, res) {
-        res.sendFile(path.resolve() + "/public/html/articles.html");
+        const article1 = {
+            title: "Random Article",
+            author: "Beepoop",
+            text: "This is a random article",
+            date: "Febuary 13th, 2034",
+        };
+
+        const article2 = {
+            title: "Gahhh",
+            author: "Moots",
+            text: "This is a random grumble page",
+            date: "Febuary 13th, 2024",
+        };
+
+        const articles = [article1, article2];
+
+
+        res.render("articles", {articles});
+        //res.sendFile(path.resolve() + "/public/html/articles.html");
     }
 
     static getProfile(req, res) {
