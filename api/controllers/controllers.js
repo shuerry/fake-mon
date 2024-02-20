fa4dad626c5b65a47fccd5dbf1ebdc63464196ff
greatestblueheron <s.xu.nature@gmail.com>
2024-02-20 18:01:45 -0500

    added database stuff, currently crashes (because no data)

--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -1,29 +1,14 @@
 import path from "path";
-
+import PokemonAccessor from "../models/pokemon.js";
 class PageController {
     static getHomePage(req, res) {
         res.sendFile(path.resolve() + "/public/html/index.html");
     }
 
-    static getArticles(req, res) {
-        const article1 = {
-            title: "Random Article",
-            author: "Beepoop",
-            text: "This is a random article",
-            date: "Febuary 13th, 2034",
-        };
+    static async getArticles(req, res) {
+        const pokemon = await PokemonAccessor.getAllPokemon();
 
-        const article2 = {
-            title: "Gahhh",
-            author: "Moots",
-            text: "This is a random grumble page",
-            date: "Febuary 13th, 2024",
-        };
-
-        const articles = [article1, article2];
-
-
-        res.render("articles", {articles});
+        res.render("articles", {pokemon});
         //res.sendFile(path.resolve() + "/public/html/articles.html");
     }
 
---
+++ api/db/pokemon_accessor.js
@@ -0,0 +1,17 @@
+import Pokemon from "../models/pokemon.js";
+import mongoose from "mongoose";
+import Connection from "./connection";
+
+export default class PokemonAccessor {
+    async getPokemonByName(pokeName) {
+        await Connection.open("pokemons");
+        const pokemonFound = await Pokemon.findOne({name: pokeName});
+        return pokemonFound;
+    }
+
+    async getAllPokemon() {
+        await Connection.open("pokemons");
+        const allPokemon = await Pokemon.find();
+        return allPokemon;
+    }
+}
